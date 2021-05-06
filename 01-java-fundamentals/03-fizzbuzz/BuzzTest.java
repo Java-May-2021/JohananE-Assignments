97fc72773a9d8e753f18d6c58dac45e242323d57
jedmeade2011 <77906022+jedmeade2011@users.noreply.github.com>
2021-05-06 11:22:57 -0700

    Add files via upload

---
+++ 01-java-fundamentals/03-fizzbuzz/BuzzTest.java
@@ -0,0 +1,7 @@
+public class BuzzTest {
+    public static void main(String[] args){
+        Buzz test = new Buzz();
+        String num = test.fizzBuzz(25);
+        System.out.println(num);
+    }
+}
