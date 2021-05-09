d5c0cb688b39c285baa2df7697a17fbff7bba738
jedmeade2011 <77906022+jedmeade2011@users.noreply.github.com>
2021-05-09 08:00:36 -0700

    Delete 01-java-fundamentals/03-fizzbuzz directory

--- 01-java-fundamentals/03-fizzbuzz/BuzzTest.java
+++
@@ -1,7 +0,0 @@
-public class BuzzTest {
-    public static void main(String[] args){
-        Buzz test = new Buzz();
-        String num = test.fizzBuzz(25);
-        System.out.println(num);
-    }
-}
